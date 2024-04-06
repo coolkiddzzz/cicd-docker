1de0a896357af49e2d9d30a8204e2c2167dcda6d
coolkiddzzz <mwaiyee96@gmail.com>
2024-04-06 16:31:37 +0800

    edit CICD files

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_ecs_task_definition" "service" {
 
   runtime_platform {
     operating_system_family = "LINUX"
-    cpu_architecture        = "ARM64"
+    cpu_architecture        = "AMD64"
   }
   
   container_definitions = jsonencode([
